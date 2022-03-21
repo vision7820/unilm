41cf6a68fc687ed340db74c075d38de6d9c8db90
Han Sheng <hanshengliang@outlook.com>
2022-03-22 00:26:39 +0800

    add setup.sh

---
+++ dit/setup.sh
@@ -0,0 +1,4 @@
+pip3 install torch==1.11.0+cu113 torchvision==0.12.0+cu113 torchaudio==0.11.0+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
+python3 -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
+pip3 install timm
+pip install opencv-python
